84c6695a6c6a33101ed7ab886217f898e7d6a1f8
cocucku13 <themeppci@yandex.ru>
2024-09-18 20:24:50 +0300

    yandexgpt init

---
+++ ml/gpt.py
@@ -0,0 +1,36 @@
+import requests
+import os
+import json
+
+
+class GPT:
+    def __init__(self):
+        self.YC = os.getenv('YC_TOKEN')
+        self.folder = 'b1gchek74cd5e8aadsp6'
+        self.url = 'https://llm.api.cloud.yandex.net/foundationModels/v1/completion'
+
+    def generate(self, prompt):
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self.YC}",
+            "X-folder-id": self.folder
+        }
+
+        data = {
+            "modelUri": f"gpt://{self.folder}/yandexgpt-lite",
+            "completionOptions": {
+                "stream": False,
+                "temperature": 0.6,
+                "maxTokens": 2000
+            },
+            "messages": [
+                {
+                    "role": "user",
+                    "text": prompt
+                }
+            ]
+        }
+
+        response = requests.post(self.url, headers=headers, data=json.dumps(data))
+
+        return response.json()['result']['alternatives'][0]['message']['text']
