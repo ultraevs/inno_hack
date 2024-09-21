58be1c11274fb4ea24602c4f38cb0ac8da1a82f9
cocucku13 <themeppci@yandex.ru>
2024-09-21 15:34:57 +0300

    move

--- ml/gpt.py
+++ ml/gpt.py
@@ -5,7 +5,8 @@ import json
 
 class GPT:
     def __init__(self):
-        self.YC = os.getenv('YC_TOKEN')
+        #self.YC = os.getenv('YC_TOKEN')
+        self.YC = 't1.9euelZqSlpLLlsyLipOPzo6JjJOXj-3rnpWaisjJxsyJy5LLkM2byovJzs7l8_dQBkVI-e8GUBER_d3z9xA1Qkj57wZQERH9zef1656VmpWLlMmUk8_Gm5rGk8mWj5Ce7_zF656VmpWLlMmUk8_Gm5rGk8mWj5Ce.WHSmxFZRhEx6GlB9HIx0Q6rK0-nLnGtIUy99WlmWXkM8mdlxH-ENNYMaEhRl1v_Rqu8C2KD3sDZ4xDfmzfsCDg'
         self.folder = 'b1gchek74cd5e8aadsp6'
         self.url = 'https://llm.api.cloud.yandex.net/foundationModels/v1/completion'
 
