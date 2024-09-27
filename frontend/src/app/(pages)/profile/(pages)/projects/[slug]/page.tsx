4fde537f53432a0a198355ed7aa7c1e0c21aee4e
Konstantin <kostya1703tsoy@gmail.com>
2024-09-27 07:05:19 +0300

    create sections in project page

--- frontend/src/app/(pages)/profile/(pages)/projects/[slug]/page.tsx
+++ frontend/src/app/(pages)/profile/(pages)/projects/[slug]/page.tsx
@@ -1,20 +1,53 @@
+"use client";
+
 import React from "react";
 import styles from "./page.module.scss";
-import { EditableTable } from "@/components/EditableTable";
-import { FontOptions } from "@/components/FontOptions";
+import { notFound, useSearchParams } from "next/navigation";
+import { Tables } from "./sections/Tables";
+import { ProjectData } from "./sections/ProjectData";
+import { routes } from "@/consts/routes";
+import Link from "next/link";
 
 export default function ProjectSlug({
   params: { slug },
 }: {
   params: { slug: string };
 }) {
+  const searchParams = useSearchParams();
+  const sectionId = searchParams.get("section") || "1";
+
+  const renderSection = () => {
+    switch (sectionId) {
+      case "1":
+        return <ProjectData />;
+      case "2":
+        return <Tables />;
+      default:
+        return notFound();
+    }
+  };
+
+  const link =
+    sectionId === "1"
+      ? {
+          title: "К таблице задач",
+          link: `${routes.profile.projects}/${slug}?section=2`,
+        }
+      : {
+          title: "К проекту",
+          link: `${routes.profile.projects}/${slug}?section=1`,
+        };
+
+  const role = "frontend";
   return (
     <div className={styles.page}>
-      <h1>Project {slug}</h1>
-      <div className={styles.page__table}>
-        <EditableTable />
-        <FontOptions />
-      </div>
+      <ul>
+        <li>
+          <Link href={link.link}>{link.title}</Link>
+        </li>
+        <li>{role}</li>
+      </ul>
+      <div className={styles.page__content}>{renderSection()}</div>
     </div>
   );
 }
