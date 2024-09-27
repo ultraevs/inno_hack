f23148b3a463265a1b46c3b9af365558939ae5c4
Konstantin <kostya1703tsoy@gmail.com>
2024-09-27 07:25:36 +0300

    create notifications page

--- frontend/src/app/(pages)/profile/(pages)/notifications/page.tsx
+++ frontend/src/app/(pages)/profile/(pages)/notifications/page.tsx
@@ -1,7 +1,29 @@
+"use client";
+
+import React from "react";
+import styles from "./page.module.scss";
+import { Notification } from "@/components/Notification";
+
 export default function Notifications() {
+  const count = 1;
+
   return (
-    <div>
-      <h1>Notifications Page</h1>
+    <div className={styles.page}>
+      {!count ? (
+        <div className={styles.page__noData}>
+          <h2>У вас пока нет уведомлений</h2>
+        </div>
+      ) : (
+        <div className={styles.page__content}>
+          <ul>
+            <Notification
+              item={{
+                text: "Crowdy#2280 приглашает Вас в проект Selectel hack",
+              }}
+            />
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
